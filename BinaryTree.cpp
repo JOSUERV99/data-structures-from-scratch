4415779385dc113fe7d195d1d0612530f06481f8
josuerv99 <josuerojasvega@gmail.com>
2020-05-12 01:09:30 -0600

    <TreeNode, BinaryTree>: creating nodes, #bug insert function is not saving changes

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,7 +1,63 @@
 template <class T>
 class BinaryTree {
-public:
-
 private:
+	int nodesAmount;
+	TreeNode<T> *root;
 
+	void _insert(TreeNode<T>*, TreeNode<T>*, TreeNode<T>*);
+	void _remove(TreeNode<T>*, TreeNode<T>*, TreeNode<T>*);
+	void _preOrder(TreeNode<T>*, LinkedList<T>**);
+
+public:
+	BinaryTree(): root(nullptr), nodesAmount(0) {}
+
+	void insert(T value);
+	void remove(T value);
+	
+	LinkedList<T> *preOrder();
+	LinkedList<T> inOrder();
+	LinkedList<T> postOrder();
+
+	int deep(T value);
+	bool contains(T value);
 };
+
+template <class T>
+void BinaryTree<T>::insert(T value) {	
+	_insert(root, nullptr, new TreeNode<T>(value));
+}
+
+template <class T>
+void BinaryTree<T>::_insert(TreeNode<T>* root, TreeNode<T>* parent, TreeNode<T>* value) {	
+
+	if (!root) {
+		root = value;
+		root->parent = parent;
+		nodesAmount++;
+		return;
+	}
+
+	if (root->getValue() < value->getValue()) 
+		_insert( root->right, root, value );
+	else 
+		_insert( root->left, root, value );
+}
+
+template <class T>
+LinkedList<T> *BinaryTree<T>::preOrder() {
+	LinkedList<T> *list = new LinkedList<T>();
+	_preOrder(root, &list);
+	return list;
+}
+
+template <class T>
+void BinaryTree<T>::_preOrder(TreeNode<T>* root, LinkedList<T> **list) {
+	if (!root) {
+		return;
+	} else {
+		_preOrder(root->left, list);
+		(*list)->push_back(root->getValue());
+		_preOrder(root->right, list);
+		std::cout << (*list)->size();
+	}
+}
--- TreeNode.cpp
+++ TreeNode.cpp
@@ -1,7 +1,19 @@
 template <class T>
 class TreeNode {
 public:
+	// primitive values
+	T value;
+	TreeNode<T> *right, *left, *parent;
+	
+	// builders
+	TreeNode(T value): value(value), right(nullptr), left(nullptr), parent(nullptr) {}
 
-private:
+	// methods
+	T getValue() { return this->value; }
 
+	//overwriting operators
+	friend ostream& operator << (ostream &o,const TreeNode<T> &p) {
+    	o << p.getValue() << ", ";
+    	return o;
+	}
 };
